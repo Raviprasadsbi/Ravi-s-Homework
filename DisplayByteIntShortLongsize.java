a72c55bdfcd854acee0058315ef1e9e10a1768fc
Raviprasadsbi <102477795+Raviprasadsbi@users.noreply.github.com>
2022-05-04 10:19:16 +0530

    Add files via upload

---
+++ DisplayByteIntShortLongsize.java
@@ -0,0 +1,31 @@
+
+public class DisplayByteIntShortLongsize {
+
+	public static void main(String[] args) {
+		System.out.println("Byte min Value:"+Byte.MIN_VALUE);
+		System.out.println("Byte max Value:"+Byte.MAX_VALUE);
+
+		System.out.println("Short min Value:"+Short.MIN_VALUE);
+		System.out.println("Short max Value:"+Short.MAX_VALUE);
+		
+		System.out.println("Long min Value:"+Long.MIN_VALUE);
+		System.out.println("Long max Value:"+Long.MAX_VALUE);
+		
+		System.out.println("int min Value:"+Integer.MIN_VALUE);
+		System.out.println("int max Value:"+Integer.MAX_VALUE);
+		
+		System.out.println("Float min Value:"+Float.MIN_VALUE);
+		System.out.println("Float max Value:"+Float.MAX_VALUE);
+		
+		System.out.println("Double min Value:"+Double.MIN_VALUE);
+		System.out.println("Double max Value:"+Double.MAX_VALUE);
+		
+		System.out.println("Char min Value:"+Character.MIN_VALUE);
+		System.out.println("Char max Value:"+Character.MAX_VALUE);
+		
+	for(int index=0;index<255;index++) {
+			System.out.println(index+"The char is:"+(char)index);
+		
+}
+}
+}
---
+++ Greetings.java
@@ -0,0 +1,17 @@
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+		Joker jk = new Joker();
+		jk.print();
+
+	}
+
+}
+
+class Joker {
+	public void print() {
+		System.out.println("Hello   ");
+	}
+}
